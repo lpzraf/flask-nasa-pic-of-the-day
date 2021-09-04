05dbbfaeda8d81676904fa0ce403f1c3415de2d1
Rafael Lopez <lpz.raf@gmail.com>
2021-09-03 22:53:04 -0400

    Added css for text and image.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, json
+import requests
+import credentials
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    api_key = credentials.API_KEY
+    req = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + api_key)
+    data = json.loads(req.content)
+    return render_template('index.html', data=data)
+
+
+@app.route("/about")
+def about():
+    api_key = credentials.API_KEY
+    req = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + api_key)
+    data = json.loads(req.content)
+    return render_template('about.html', data=data)
 
 
 if __name__ == "__main__":
