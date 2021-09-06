c8f41ec6c391577fada5b724f4e2553add91e5e0
Rafael Lopez <lpz.raf@gmail.com>
2021-09-06 11:03:11 -0400

    finished the app hosting it in Heroku.

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def index():
     return render_template('index.html', data=data)
 
 
-@app.route("/about", methods=['GET', 'POST'])
-def about():
+@app.route("/image-archive", methods=['GET', 'POST'])
+def archive():
     if request.method == "POST":
         start_date = request.form.get('start')
         end_date = request.form.get('end')
@@ -32,12 +32,12 @@ def about():
                            api_key + "&start_date=" + start_date +
                            "&end_date=" + end_date)
         data = json.loads(req.content)
-        return render_template('about.html', data=data)
+        return render_template('archive.html', data=data)
 
     api_key = credentials.API_KEY
     req = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + api_key)
     data = json.loads(req.content)
-    return render_template('about.html', data=data)
+    return render_template('archive.html', data=data)
 
 
 if __name__ == "__main__":
